8476f7f06f49f07c9ae7d46d76ed44d15ec30b2b
Asma Abu Saa <asmaabusaa4@gmail.com>
2023-01-16 17:02:19 +0200

    Step 3

--- StringCalculator.Tests/StringCalculatorTests.cs
+++ StringCalculator.Tests/StringCalculatorTests.cs
@@ -28,12 +28,21 @@ namespace StingCalculator.Tests
             result = calculator.Add("1,2");
             Assert.Equal(3, result);
         }
+
         [Fact]
         public void Add_UnKnownAmountOfNumbers_RetrunTheirSum()
         {
             result = calculator.Add("1,2,4,5,9");
             Assert.Equal(1+2+4+5+9,result);
         }
+
+        [Fact]
+        public void Add_StringofNumbersWithHandlingnewLine_RetrunTheirSum()
+        {
+            result = calculator.Add("1\n11,2");
+            Assert.Equal(14, result);
+        }
+
 
 
 
--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,17 +1,38 @@
-﻿namespace String_Calculator
+﻿using System;
+using System.Text;
+
+namespace String_Calculator
 {
     public class StringCalculator
     {
         public int Add(string numbers)
         {
+            StringBuilder temp = new StringBuilder();
+            numbers = numbers.Replace("\n", ",");
             int sum = 0;
-            foreach (var num in numbers)
+            for (int num = 0; num < numbers.Length; num++)
             {
-                if (num >= 48 && num <= 57)
-                    sum += num - 48;
+
+                if (numbers[num] >= 48 && numbers[num] <= 57)
+                    temp.Append(numbers[num]);
+
+                if (numbers[num].Equals(',') || num == numbers.Length - 1)
+                {
+                    try
+                    {
+                        int val;
+                        val = Int32.Parse(temp.ToString());
+                        sum += val;
+                        temp.Clear();
+                    }
+
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Can't Parsed '{0}'", temp);
+                    }
+                }
             }
             return sum;
         }
-
     }
 }
